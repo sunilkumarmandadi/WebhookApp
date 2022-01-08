1c3689a0d518302fc4033ac3109d8cc1353da020
sunilkumarmandati <sunilkumar.mandadi@gmail.com>
2022-01-07 17:24:52 -0800

    initial commit

---
+++ AirlineWeb/Models/FlightDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineWeb.Models
+{
+    public class FlightDetail
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string FlightCode { get; set; }
+
+        [Column(TypeName = "decimal(6,2")]
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
