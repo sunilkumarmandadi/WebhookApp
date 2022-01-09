a3b7e03790c08c26d68f01d4400b93ee8fab5f3c
sunilkumarmandati <sunilkumar.mandadi@gmail.com>
2022-01-09 13:43:41 -0800

    Message bus client

---
+++ AirlineWeb/Dtos/NotificationMessageDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineWeb.Dtos
+{
+    public class NotificationMessageDto
+    {
+        public NotificationMessageDto()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; }
+        public string WebhookType { get; set; }
+        public string FlightCode { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal NewPrice { get; set; }
+    }
+}
--- TravelAgentAPI/Controllers/NotificationsController.cs
+++ TravelAgentAPI/Controllers/NotificationsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TravelAgentAPI.Data;
+using TravelAgentAPI.Dtos;
 
 namespace TravelAgentAPI.Controllers
 {
@@ -18,6 +19,30 @@ namespace TravelAgentAPI.Controllers
         {
             _context = context;
         }
+        [HttpPost]
+        public ActionResult FlightChanged(FlightDetailUpdateDto flightDetailUpdateDto)
+        {
+            Console.WriteLine($"Webhook Receieved from: {flightDetailUpdateDto.Publisher}");
 
+            var secretModel = _context.SubscriptionSecrets.FirstOrDefault(s =>
+                s.Publisher == flightDetailUpdateDto.Publisher &&
+                s.Secret == flightDetailUpdateDto.Secret);
+
+            if (secretModel == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Secret - Ignore Webwook");
+                Console.ResetColor();
+                return Ok();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valid Webhook!");
+                Console.WriteLine($"Old Price {flightDetailUpdateDto.OldPrice}, New Price {flightDetailUpdateDto.NewPrice}");
+                Console.ResetColor();
+                return Ok();
+            }
+        }
     }
 }
